f755ca88406d6735f3c801a5ab6904978c79dbcd
keshavankit89@gmail.com <keshavankit89@gmail.com>
2021-07-03 01:50:12 +0530

    first commit

---
+++ src/main/java/in/bishwas/ankit/portfolio/controller/HomeController.java
@@ -0,0 +1,15 @@
+package in.bishwas.ankit.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+
+}
